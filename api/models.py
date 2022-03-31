12e9f35ac8f067453b6929d6ba5cd8454955fa01
doyeoo <ehdusla013@gmail.com>
2022-04-01 02:11:23 +0900

    Profile 팔로워 팔로잉 필드 추가

--- api/models.py
+++ api/models.py
@@ -4,12 +4,15 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     nickname=models.CharField(max_length=50)
-    image=models.ImageField()
-    website=models.TextField()
-    bio=models.TextField()
+    image=models.ImageField(null=True, blank=True)
+    website=models.TextField(null=True, blank=True)
+    bio=models.TextField(null=True, blank=True)
+    follower=models.ManyToManyField('self',null=True, blank=True)
+    following=models.ManyToManyField('self',null=True, blank=True)
 
     def __str__(self):
         return self.nickname
+
 
 class Post(models.Model):
     author=models.ForeignKey(User, on_delete=models.CASCADE)
@@ -31,6 +34,7 @@ class Comment(models.Model):
     post=models.ForeignKey(Post, on_delete=models.CASCADE)
     upload_at=models.DateTimeField(auto_now=True)
     content=models.TextField()
+
 
 class File(models.Model):
     post=models.ForeignKey(Post, on_delete=models.CASCADE)
