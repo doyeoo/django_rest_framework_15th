d686f6bbbda6a17bec9229c86c25ca5c059f86a2
doyeoo <ehdusla013@gmail.com>
2022-04-01 01:48:57 +0900

    Post, Like, Comment, File 모델 추가

--- api/admin.py
+++ api/admin.py
@@ -3,3 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(File)
--- api/models.py
+++ api/models.py
@@ -10,3 +10,28 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.nickname
+
+class Post(models.Model):
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    content=models.TextField()
+    upload_at=models.DateTimeField(auto_now=True)
+    likes=models.PositiveIntegerField()
+    comments=models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.content
+
+class Like(models.Model):
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    upload_at=models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    upload_at=models.DateTimeField(auto_now=True)
+    content=models.TextField()
+
+class File(models.Model):
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    file=models.FileField()
