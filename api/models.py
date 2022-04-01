092be211df0af335ad602bea92512ab0adc572c3
doyeoo <ehdusla013@gmail.com>
2022-04-01 23:24:50 +0900

    Profile 필드 수정

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname=models.CharField(max_length=50)
+    phone=models.CharField(max_length=50)
     image=models.ImageField(null=True, blank=True)
     website=models.TextField(null=True, blank=True)
     bio=models.TextField(null=True, blank=True)
