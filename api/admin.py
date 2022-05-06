5da838dee8afcccd47ba10e81b16e94b58d14d42
doyeoo <ehdusla013@gmail.com>
2022-05-06 18:38:57 +0900

    refactor customize admin

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Comment)
-admin.site.register(File)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'phone']
+    list_display_links = ['id', 'user']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'content']
+    list_display_links = ['id', 'content']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'get_post']
+    list_display_links = ['id', 'get_post']
+
+    def get_post(self, obj):
+        return obj.post.content
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'get_post']
+    list_display_links = ['id', 'get_post']
+
+    def get_post(self, obj):
+        return obj.post.content
