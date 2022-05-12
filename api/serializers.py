72ac8f9892dbf52000108550a65b8cffde820394
doyeoo <ehdusla013@gmail.com>
2022-05-13 00:16:42 +0900

    add filters

--- api/serializers.py
+++ api/serializers.py
@@ -1,30 +1,41 @@
 from rest_framework import serializers
 from .models import *
 
+
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = '__all__'
 
+
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = '__all__'
 
+
 class PostSerializer(serializers.ModelSerializer):
-    comment=CommentSerializer(many=True, read_only=True)
-    like=LikeSerializer(many=True, read_only=True)
-    file=FileSerializer(many=True, read_only=True)
+    comment = CommentSerializer(many=True, read_only=True)
+    like = LikeSerializer(many=True, read_only=True)
+    file = FileSerializer(many=True, read_only=True)
+
     class Meta:
         model = Post
         fields = '__all__'
 
+
 class ProfileSerializer(serializers.ModelSerializer):
+    def get_username(self, obj):
+        return obj.user.username
+
+    username = serializers.SerializerMethodField("get_username")
+
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = ('id', 'username', 'phone', 'image_url', 'website', 'bio', 'follower', 'following')
--- api/views.py
+++ api/views.py
@@ -1,11 +1,50 @@
 from .models import *
 from .serializers import *
 from rest_framework import viewsets
+from rest_framework import permissions
+from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ProfileUpdatePermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        id = request.GET['id']
+        blocked = Profile.objects.filter(id=id).exists()
+        return not blocked
+
+
+class PostFilter(FilterSet):
+    user = filters.NumberFilter(field_name='user')
+    like_count = filters.NumberFilter(field_name='like_count', lookup_expr='gt')
+
+    class Meta:
+        model = Post
+        fields = ['user', 'like_count']
+
+
+class ProfileFilter(FilterSet):
+    username = filters.CharFilter(field_name='username', method='filter_username')
+    image = filters.BooleanFilter(field_name='image_url', lookup_expr='isnull')
+
+    class Meta:
+        model = Profile
+        fields = ['username', 'image']
+
+    def filter_username(self, queryset, name, value):
+        filtered_queryset = queryset.filter(user__username__contains=value)
+        return filtered_queryset
+
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filter_class = PostFilter
+
 
 class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filter_class = ProfileFilter
+
